71fc1d1c93c7cfe92f75d7c6c335b4c850997e46
askrinihad <hn_askri@esi.dz>
2024-03-19 15:39:27 +0100

    plantnet API

---
+++ server/main.py
@@ -0,0 +1,90 @@
+from flask import Flask, request, jsonify
+import werkzeug
+import os
+import requests
+import time
+from threading import Thread
+
+app = Flask(__name__)
+
+API_KEY = "2b10pBTi7LFZ2zKYWkvYXpMae"
+results_data = []  # A list to store the results
+ # Global variable to track whether images have been processed
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    global processed_images  # Use the global variable
+    if request.method == "POST":
+        imagefile = request.files['image']
+        filename = werkzeug.utils.secure_filename(imagefile.filename)
+        imagefile.save("./images/" + filename)
+
+        # Starting a new thread to run the predict function
+        Thread(target=predict).start()
+        time.sleep(3) 
+        return jsonify({
+            "message": "Image uploaded successfully",
+            "results": results_data  # Include the results in the response
+        })
+
+def predict():
+    did=True
+    global results_data, processed_images 
+    results_data = []
+    processed_images = False  # Use the global variables
+    folder_path = "images"
+    api_endpoint = f"https://my-api.plantnet.org/v2/identify/all?api-key={API_KEY}"
+    image_paths = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(('.jpeg', '.jpg', '.png', '.bmp', '.webp'))]
+
+    if len(image_paths) == 0:
+        print("No images were found in the folder.")
+        exit()
+
+    results = []
+
+    # Process each image file only if images have not been processed yet
+    if not processed_images:
+        for image_path in image_paths:
+            with open(image_path, 'rb') as image_file:
+                files = {'images': image_file}
+                response = requests.post(api_endpoint, files=files)
+                json_result = response.json()
+                results.append(json_result)
+                print(json_result)
+
+             # Delay for 3 seconds between each image processing
+
+            # Delete the processed image file
+            try:
+                os.remove(image_path)
+                print(f"Deleted image: {image_path}")
+            except Exception as e:
+                print(f"Error deleting image {image_path}: {e}")
+
+        # Set the flag to indicate that images have been processed
+        processed_images = True
+
+    for i, result in enumerate(results):
+        image_path = image_paths[i]
+        for r in result.get('results', []):
+            try:
+                scientific_name = r['species']['scientificName']
+                score = r['score']
+                common_names = ', '.join(r['species']['commonNames'])
+            except KeyError:
+                scientific_name = 'Unavailable'
+                score = 'Unavailable'
+                common_names = 'Unavailable'
+          
+            if(did==True):
+                print("scientific name", scientific_name)
+                print("score:", score)
+                did=False
+                results_data.append({
+                "scientific_name": scientific_name,
+                "score": score,
+                })
+            
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
