f2eed2b30134f52761d59b82aee700654a49936a
L3ChatNoir <108089371+L3ChatNoir@users.noreply.github.com>
2024-05-03 15:29:22 +0200

    API Improvement

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-import os, requests, time
+import os, shutil, requests, time
 from threading import Thread
 from roboflow import Roboflow
 from werkzeug.utils import secure_filename
@@ -9,33 +9,83 @@ from flask import Flask, request, jsonify
 #*************************************
 API_KEY = "2b10pBTi7LFZ2zKYWkvYXpMae"
 ALLOWED_EXTENSIONS = ('.png', '.jpg', '.jpeg') # Only a tuple, Don't forget '.' before extension
-UPLOAD_FOLDER = 'birds'
-
-results_data = []  
-bird_result =[]# A list to store the results
+UPLOAD_FOLDER = 'images' # WARNING : This folder will be delete, choose a empty folder
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
+
+results_data = []  # List to shared data between Thread and caller
+
+def image_recognition(type: str, request : request)-> list[dict, ...]:
+    '''
+    Recognize the specie of type of living being in image
+    Take a Flask request with file and return a dict.
+    
+    :param type: 'plant' or 'bird' 
+    :param request: flask resquest with files and KEY = 'image'
+    :return: Empty list if can't recognize (Maybe you set wrong type) else a list of dict
+    '''
+    try:
+        global results_data
+        results_data = [] # Clean result, use for Thread share result
+        imagefile = request.files['image']# Get image from Post
+        
+        if imagefile.filename == '':
+            return {"error": "No selected file"}
+
+        if not imagefile.filename.lower().endswith(app.config['ALLOWED_EXTENSIONS']):
+            return {"error" : "Extension file not allowed"}
+
+        filename = secure_filename(imagefile.filename) 
+        imagefile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        # Select Recognition way
+        match type:
+            case 'plant':
+                recognition = Thread(target=plant_recognition)
+            case 'bird':
+                recognition = Thread(target=bird_recognition, args=[os.path.join(app.config['UPLOAD_FOLDER'],filename)])
+            case _:
+                return {"error" : "Type not found"}
+        
+        recognition.start(), # start recognition
+        recognition.join() # Wait recognition
+
+        # Delete images
+        shutil.rmtree(path=app.config['UPLOAD_FOLDER'], ignore_errors=True)
+        os.mkdir(app.config['UPLOAD_FOLDER'])
+        
+        return results_data
+
+    except KeyError:
+        return {"error": "No file part"}
+    except FileNotFoundError:
+        if not os.path.isdir(app.config['UPLOAD_FOLDER']):
+            os.mkdir(app.config['UPLOAD_FOLDER'])
+        return {"error" : "implementation skill issue, Please retry. If retry not work, please contact a developer"}
+    except Exception as error:
+        app.logger.error(error)
+        return {"error" : "Internal Error or Exception not implemented"}
 
 @app.route('/upload', methods=["POST"])
-def upload():
-    global results_data
-    results_data = []
-    imagefile = request.files['image']
-    filename = secure_filename(imagefile.filename)
-    imagefile.save("./images/" + filename)
-
-    # Starting a new thread to run the predict function
-    prediction = Thread(target=predict)
-    prediction.start()
-    prediction.join()
+def upload_plant():
     return jsonify({
         "message": "Image uploaded successfully",
-        "results": results_data  # Include the results in the response
+        "results": image_recognition(type='plant', request= request)
     })
 
-def predict():
+@app.route('/bird_recognition', methods=["POST"])  
+def upload_bird():
+    return jsonify({
+        "message": "Bird image uploaded successfully",
+        "results": image_recognition(type='bird', request= request)  # Include the results in the response
+    })
+
+def plant_recognition():
+    # TODO : Refactor, use only 1 image
     global results_data
+    processed_images = False
     folder_path = "images"
     api_endpoint = f"https://my-api.plantnet.org/v2/identify/all?api-key={API_KEY}"
     image_paths = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(('.jpeg', '.jpg', '.png', '.bmp', '.webp'))]
@@ -52,14 +102,6 @@ def predict():
             response = requests.post(api_endpoint, files=files)
             json_result = response.json()
             results.append(json_result)
-
-        '''
-        # Delete the processed image
-        try:
-            os.remove(image_path)
-        except Exception as e:
-            app.logger.error(f"Error deleting image {image_path}: {e}")
-        '''
 
     for i, result in enumerate(results):
         image_path = image_paths[i]
@@ -78,34 +120,8 @@ def predict():
                 "score": score,
                 })
 
-@app.route('/bird_recognition', methods=["POST"])  
-def upload_bird():
-    if 'image' not in request.files:
-        return jsonify({"error": "No file part"})
-
-    file = request.files['image']
-
-    if file.filename == '':
-        return jsonify({"error": "No selected file"})
-
-    if file.filename.lower().endswith(ALLOWED_EXTENSIONS):
-        filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
-
-        # Starting a new thread to run the bird_recognition function
-        prediction = Thread(target=bird_recognition, args=(file_path,))
-        prediction.start()
-        prediction.join()
-        return jsonify({
-            "message": "Bird image uploaded successfully",
-            "results": bird_result  # Include the results in the response
-        })
-
-    return jsonify({"error" : "Extension file not allowed"})
-
 def bird_recognition(image_path : str):
-    global bird_result
+    global results_data
     rf = Roboflow(api_key="zil5scCJofh6YkIUGFAO")
     project = rf.workspace().project("bird-v2")
     model = project.version(2).model
@@ -121,7 +137,7 @@ def bird_recognition(image_path : str):
         class_name = prediction.get('class', None)
 
     # Construct the result
-    bird_result = {
+    results_data = {
         "confidence": confidence,
         "class_name": class_name,
         "predicted_image_path": prediction_path
