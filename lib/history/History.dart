d7bf38f47ea234f322536b67eb0332839cfe3f03
L3ChatNoir <108089371+L3ChatNoir@users.noreply.github.com>
2024-05-13 11:23:31 +0200

    Uppercase file name fix

--- lib/history/History.dart
+++ lib/history/History.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_app/history/historymap.dart';
+import 'package:test_app/history/HistoryMap.dart';
 import 'package:test_app/observation/ObservationList.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -112,14 +112,14 @@ class _HistoryState extends State<History> {
                         fixedSize: Size(150, 50),
                       ),
                       onPressed: () {
-                         Navigator.push(
-                           context,
-                           MaterialPageRoute(
-                             builder: (context) => HistoryMap(
-                                 typeObs: dropdownValue,
-                                 aeroport: widget.aeroport),
-                           ),
-                         );
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HistoryMap(
+                                typeObs: dropdownValue,
+                                aeroport: widget.aeroport),
+                          ),
+                        );
                       },
                       child: Text(
                         AppLocalizations.of(context)!.localiser,
