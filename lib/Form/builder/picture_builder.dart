06b6e78fb85ce7a96318f7391ab6a0403a1aa8e8
L3ChatNoir <108089371+L3ChatNoir@users.noreply.github.com>
2024-05-21 11:56:27 +0200

    Importer Photo Style

--- lib/Form/builder/picture_builder.dart
+++ lib/Form/builder/picture_builder.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
+import 'package:test_app/style/StyleText.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 class PictureWidget extends StatefulWidget {
   @override
   _PictureWidgetState createState() => _PictureWidgetState();
@@ -11,46 +14,68 @@ class _PictureWidgetState extends State<PictureWidget> {
   XFile? _image;
 
   Future getImage() async {
-  final ImageSource? source = await showDialog<ImageSource>(
-    context: context,
-    builder: (BuildContext context) {
-      return SimpleDialog(
-        title: const Text('Choisir une source'),
-        children: <Widget>[
-          SimpleDialogOption(
-            onPressed: () {
-              Navigator.pop(context, ImageSource.camera);
-            },
-            child: const Text('Prendre une photo'),
-          ),
-          SimpleDialogOption(
-            onPressed: () {
-              Navigator.pop(context, ImageSource.gallery);
-            },
-            child: const Text('Galerie'),
-          ),
-        ],
-      );
-    },
-  );
+    final ImageSource? source = await showDialog<ImageSource>(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Choisir une source'),
+          children: <Widget>[
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.pop(context, ImageSource.camera);
+              },
+              child: const Text('Prendre une photo'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.pop(context, ImageSource.gallery);
+              },
+              child: const Text('Galerie'),
+            ),
+          ],
+        );
+      },
+    );
 
-  if (source != null) {
-    final image = await ImagePicker().pickImage(source: source);
+    if (source != null) {
+      final image = await ImagePicker().pickImage(source: source);
 
-    setState(() {
-      _image = image;
-    });
+      setState(() {
+        _image = image;
+      });
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Text('Ajoutez une photo'),
-        ElevatedButton(
-          onPressed: getImage,
-          child: const Text('Ajouter'),
+        SizedBox(
+          width: 220,
+          child: RawMaterialButton(
+            fillColor: const Color(0xFF006766),
+            elevation: 0.0,
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            onPressed: getImage,
+            child: Row(
+              children: [
+                const SizedBox(width: 10),
+                Icon(
+                  Icons.photo_library,
+                  size: 28,
+                  color: Colors.white,
+                ),
+                const SizedBox(width: 20), // Add spacing between icon and text
+                Text(
+                  AppLocalizations.of(context)!.importerPhoto,
+                  style: StyleText.getButton(),
+                ),
+              ],
+            ),
+          ),
         ),
         if (_image != null)
           Image.file(
--- lib/Form/form_page.dart
+++ lib/Form/form_page.dart
@@ -1,61 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/form/form_builder.dart';
+import 'package:test_app/style/StyleText.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 Widget buildFormPage(BuildContext context, String jsonPath) {
   final _formKey = GlobalKey<FormState>();
 
   return FutureBuilder(
-      future: buildFormFromJson(context, jsonPath),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Column(
-            children: [
-              Expanded(
-                child: Form(
-                  key: _formKey,
-                  child: Padding(
-                    padding: const EdgeInsets.all(45.0),
-                    child: ListView.separated(
-                      itemCount: (snapshot.data as List<Widget>).length,
-                      itemBuilder: (context, index) {
-                        return (snapshot.data as List<Widget>)[index];
-                      },
-                      separatorBuilder: (context, index) {
-                        return const SizedBox(height: 13.5);
-                      },
-                    ),
+    future: buildFormFromJson(context, jsonPath),
+    builder: (context, snapshot) {
+      if (snapshot.hasData) {
+        return Column(
+          children: [
+            Expanded(
+              child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(45.0),
+                  child: ListView.separated(
+                    itemCount: (snapshot.data as List<Widget>).length,
+                    itemBuilder: (context, index) {
+                      return (snapshot.data as List<Widget>)[index];
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(height: 13.5);
+                    },
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      print('Form submitted');
-                    }
-                  },
-                  child: const Text('Submit'),
-                  style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.all<Size>(Size(200, 50)),
-                    backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF006766)),
-                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    print('Form submitted');
+                  }
+                },
+                child: Text(
+                  AppLocalizations.of(context)!.renregistrer,
+                  style: StyleText.getButton(),
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
                   ),
+                  backgroundColor: Color(0xFF006766),
+                  padding: const EdgeInsets.symmetric(vertical: 15.0),
+                  elevation: 0.0,
                 ),
               ),
-            ],
-          );
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          return const CircularProgressIndicator();
-        }
-      },
-    );
+            ),
+          ],
+        );
+      } else if (snapshot.hasError) {
+        return Text('Error: ${snapshot.error}');
+      } else {
+        return const CircularProgressIndicator();
+      }
+    },
+  );
 }
