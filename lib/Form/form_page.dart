6fe8271819ed1469da7b633d62f2f7ed0dce45bc
Pikaagante <briaultmaxence@gmail.com>
2024-05-21 10:48:46 +0200

    build form page function

--- lib/Form/form_page.dart
+++ lib/Form/form_page.dart
@@ -1,71 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:test_app/builder/form_builder.dart';
+import 'package:test_app/form/form_builder.dart';
 
-class FormPage extends StatelessWidget {
+Widget buildFormPage(BuildContext context, String jsonPath) {
   final _formKey = GlobalKey<FormState>();
 
-  FormPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Form Page'),
-      ),
-      body: FutureBuilder(
-        future: buildFormFromJson(context, 'assets/form_structure.json'), // Appel de la fonction buildFormFromJson
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Column(
-              children: [
-                Expanded(
-                  child: Form(
-                    key: _formKey,
-                    child: Padding(
-                      padding: const EdgeInsets.all(45.0),
-                      child: ListView.separated(
-                        itemCount: (snapshot.data as List<Widget>).length, // Utilisation de la liste de widgets retournée par la fonction
-                        itemBuilder: (context, index) {
-                          return (snapshot.data as List<Widget>)[index]; // Affichage des widgets
-                        },
-                        separatorBuilder: (context, index) { // Ajout d'un espace entre les widgets
-                          return const SizedBox(height: 13.5);
-                        },
+  return FutureBuilder(
+      future: buildFormFromJson(context, jsonPath),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Column(
+            children: [
+              Expanded(
+                child: Form(
+                  key: _formKey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(45.0),
+                    child: ListView.separated(
+                      itemCount: (snapshot.data as List<Widget>).length,
+                      itemBuilder: (context, index) {
+                        return (snapshot.data as List<Widget>)[index];
+                      },
+                      separatorBuilder: (context, index) {
+                        return const SizedBox(height: 13.5);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      print('Form submitted');
+                    }
+                  },
+                  child: const Text('Submit'),
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all<Size>(Size(200, 50)),
+                    backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF006766)),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) { // Validation du formulaire
-                        _formKey.currentState!.save();
-                        print('Form submitted');
-                      }
-                    },
-                    child: const Text('Submit'),
-                    style: ButtonStyle(
-                      minimumSize: MaterialStateProperty.all<Size>(Size(200, 50)),
-                      backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF006766)),
-                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}'); // Affichage d'un message d'erreur
-          } else {
-            return const CircularProgressIndicator();
-          }
-        },
-      ),
+              ),
+            ],
+          );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
     );
-  }
 }
